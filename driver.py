1c0975679afd61baf707ed1630e462ded5a4a7e0
gaurang-tandon <gaurang.tandon@gmail.com>
2024-08-29 11:22:14 +0530

    exception for negative numbers check added in code

--- driver.py
+++ driver.py
@@ -2,9 +2,16 @@ import re
 
 def add(numbers:str):
     number_list = re.split(r'[;\n,//]', numbers)
+
     number_list = [int(x.strip()) for x in number_list if x]
+
+    negative_numbers = [x for x in number_list if x < 0]
+
+    if negative_numbers:
+        raise Exception(f'negative numbers not allowed {",".join([str(x) for x in negative_numbers])}')
+
     return sum(number_list)
     
 
 
-print(add('//;\n1;2\n,\n34,,,3'))
+print(add('//;\n1;-2\n,\n-34,,,3'))
